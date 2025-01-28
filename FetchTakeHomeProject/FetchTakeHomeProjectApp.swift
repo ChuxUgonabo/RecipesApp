1db0c9a56715a65a4573d9034892c8c21234cb1a
John_Ugonabo <chuxugonabo@gmail.com>
2025-01-27 19:28:48 -0800

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Chux Ugonabo MacBook on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
