ab0bbee802980788b9143755513e61e46db7a8f2
John_Ugonabo <chuxugonabo@gmail.com>
2025-02-10 17:51:17 -0800

    Added APIService with handle all interaction with API

---
+++ FetchTakeHomeProject/Services/APIService.swift
@@ -0,0 +1,27 @@
+//
+//  APIService.swift
+//  FetchTakeHomeProject
+//
+//  Created by Chux Ugonabo MacBook on 2025-01-29.
+//
+
+import Foundation
+
+class APIService {
+    
+    private let baseURL = "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json"
+
+    func fetchRecipes() async throws -> [Recipe] {
+        guard let url = URL(string: baseURL) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return try JSONDecoder().decode([Recipe].self, from: data)
+    }
+}
