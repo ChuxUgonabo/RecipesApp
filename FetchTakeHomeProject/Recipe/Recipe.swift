13cfc546f1a35a00a9948dd63a337fc4fea40e0e
John_Ugonabo <chuxugonabo@gmail.com>
2025-02-10 17:49:45 -0800

    Add a recipe model that contain the structure/ model to be retrieve from the API

---
+++ FetchTakeHomeProject/Recipe/Recipe.swift
@@ -0,0 +1,30 @@
+//
+//  Recipe.swift
+//  FetchTakeHomeProject
+//
+//  Created by Chux Ugonabo MacBook on 2025-01-27.
+//
+
+import Foundation
+
+struct Recipe: Codable, Hashable {
+    
+    let cuisine: String
+    let name: String
+    let photoUrlLarge: String?
+    let photoUrlSmall: String?
+    let uuid: String
+    let sourceUrl: String?
+    let youtubeUrl: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case cuisine
+        case name
+        case photoUrlLarge = "photo_url_large"
+        case photoUrlSmall = "photo_url_small"
+        case uuid
+        case sourceUrl = "source_url"
+        case youtubeUrl = "youtube_url"
+    }
+    
+}
